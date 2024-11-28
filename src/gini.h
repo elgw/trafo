c66f18a9f09048541e77cea640e9517797b6da36
Erik Wernersson <erik.wernersson@gmail.com>
2024-11-28 07:10:39 +0100

    documentation improved

--- src/gini.h
+++ src/gini.h
@@ -10,10 +10,19 @@
  * feature = [0.1, 0.2, 0.2, 0.3, 0.3] the only possible outcomes are
  * nleft \in {0, 1, 3, 5}.
  *
+ * The Gini impurity, $G$, for a partition is defined as
+ *
+ * $$G = 1 - \sum p_i^2 $$
+ *
+ * where $p_i$ is the fraction of elements that belongs to class $i$
+ *
+ * 0, or "no impurity" is achieved if all elements belong to the same
+ * class, if the partition has a mix of classes this value will be larger
+ *
  * Returns:
  * the return value is the gini of the split.
- * _gleft and _gright is gini purity of the left and right partition
- * _nleft and _nright is the number of elements in the left and right partition
+ * G_left and G_right is gini purity of the left and right partition
+ * n_left and n_right is the number of elements in the left and right partition
  *
  */
 
@@ -22,7 +31,7 @@ gini_split(const u32 * restrict class,
            const f64 * restrict feature,
            const u32 npoint,
            const u32 max_label,
-           u32  * restrict _nleft,
-           u32  * restrict _nright,
-           f64* restrict _gleft,
-           f64* restrict _gright);
+           u32  * restrict n_left,
+           u32  * restrict n_right,
+           f64* restrict G_left,
+           f64* restrict G_right);
