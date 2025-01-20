71b164a1e9fc151950d5bbb00d07581a8e238d61
Erik Wernersson <erik.wernersson@gmail.com>
2025-01-20 10:17:54 +0100

    ...

--- src/trafo_util.c
+++ src/trafo_util.c
@@ -14,7 +14,8 @@
 // if typedef doesn't exist (msvc, blah)
 typedef intptr_t ssize_t;
 
-ssize_t getline(char **lineptr, size_t *n, FILE *stream) {
+static ssize_t
+getline(char **lineptr, size_t *n, FILE *stream) {
     size_t pos;
     int c;
 
